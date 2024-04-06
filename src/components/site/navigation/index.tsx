434f3b6d16b073f53a81a12a3c39347a83795401
gustcomer <gustcomer@gmail.com>
2024-04-06 20:30:32 -0300

    feat: add login button and theme switch button

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from '@/components/global/mode-toggle'
+import { UserButton } from '@clerk/nextjs'
 import { User } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -27,6 +29,16 @@ const Navigation = (props: Props) => {
           <Link href={"#"}>Features</Link>
         </ul>
       </nav>
+      <aside className="flex gap-2 items-center">
+  <Link
+    href='/agency'
+    className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
+  >
+    Login
+  </Link>
+  <UserButton />
+  <ModeToggle />
+</aside>
     </div>
   )
 }
